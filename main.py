3f146167c51e646466b7378e430ba4b0b3b0df5e
abhinavdext <163636144+abhinavdext@users.noreply.github.com>
2024-03-16 15:11:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title('Abhinav profile')
+
+st.header("To work with perseverance, measuring up to the expectation of the company and Interested to "
+             "work in team environment and contributing "
+             "to the objectives of organization that offers a challenging and opportunity as a Python Developer")
+st.subheader('Around 5+ years of relevant experience as a Web/Application Developer and coding with analytical'
+             ' programming using Python, Django.'
+             'Good Experience on Automation Testing using API Automation using Python and UI Automation '
+             'Experienced with full software development life-cycle, architecting scalable platforms, object-oriented '
+             'programming, database design and agile methodologies.')
+
+st.header('Technical skills')
+
+st.markdown('Web Frameworks = Django,Flask   \n   Web Technologies = Html,css  \n    Programming Language = Python  \nVersion Control = Git, GitHub  \n  Database = SQL, MongoDB, MySQL  \nIDEs/ Development Tool = Vs Code, PyCharm, Eclipse, Visual Studio, PyScripter  \nOperating Systems = Windows, Linux  \nTesting Tools = Postman, selenium  \nIssue Tracker = jira')
