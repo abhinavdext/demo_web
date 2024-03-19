e7f47f42622d155e387d4b3dded8c44ac4e2e9e9
abhinavdext <163636144+abhinavdext@users.noreply.github.com>
2024-03-19 21:43:57 +0530

    Update main.py
    Bmi calculator added

--- main.py
+++ main.py
@@ -21,3 +21,76 @@ st.header('Technical skills')
 
 st.markdown('Web Frameworks = Django,Flask   \n   Web Technologies = Html,css  \n    Programming Language = Python  \nVersion Control = Git, GitHub  \n  Database = SQL, MongoDB, MySQL  \nIDEs/ Development Tool = Vs Code, PyCharm, Eclipse, Visual Studio, PyScripter  \nOperating Systems = Windows, Linux  \nTesting Tools = Postman, selenium  \nIssue Tracker = jira')
 
+# BMI CALCULATOR 
+st.title('Welcome to BMI Calculator')
+
+weight = st.number_input("Enter your weight (in kgs)")
+ 
+status = st.radio('Select your height format: ',
+
+                  ('cms', 'meters', 'feet'))
+ 
+if(status == 'cms'):
+
+
+    height = st.number_input('Centimeters')
+ 
+
+    try:
+
+        bmi = weight / ((height/100)**2)
+
+    except:
+
+        st.text("Enter some value of height")
+ 
+
+elif(status == 'meters'):
+
+    height = st.number_input('Meters')
+ 
+
+    try:
+
+        bmi = weight / (height ** 2)
+
+    except:
+
+        st.text("Enter some value of height")
+ 
+
+else:
+
+    height = st.number_input('Feet')
+
+    try:
+
+        bmi = weight / (((height/3.28))**2)
+
+    except:
+
+        st.text("Enter some value of height")
+ 
+if(st.button('Calculate BMI')):
+ 
+    st.text("Your BMI Index is {}.".format(bmi))
+ 
+    if(bmi < 16):
+
+        st.error("You are Extremely Underweight")
+
+    elif(bmi >= 16 and bmi < 18.5):
+
+        st.warning("You are Underweight")
+
+    elif(bmi >= 18.5 and bmi < 25):
+
+        st.success("Healthy")
+
+    elif(bmi >= 25 and bmi < 30):
+
+        st.warning("Overweight")
+
+    elif(bmi >= 30):
+
+        st.error("Extremely Overweight")
