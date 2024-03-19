d763987c25de88f0d804f21a230c3a5b81ffbb96
abhinavdext <163636144+abhinavdext@users.noreply.github.com>
2024-03-19 22:30:19 +0530

    Update main.py
    Ok

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title('Abhinav Pandey')
-st.text('click on button')
+st.text('click to know')
 if(st.button("About")):
     #st.text("intro")
     st.markdown("To work with perseverance,measuring up to the expectation of the company and Interested to working team environment and contributing to the objectives of organization that offers a challenging and opportunity as a Python Developer.")
