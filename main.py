12631a7431aeb6322ab0f9be4812d7269ab525ca
abhinavdext <163636144+abhinavdext@users.noreply.github.com>
2024-03-19 23:08:46 +0530

    Update main.py
    Ok

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ if(st.button('Calculate BMI')):
 
 
 
-url ="https://google.com"
+url ="https://x.com/avhinavpandey?t=3aU7VK3Zelqhewv9D1158w&s=09"
 
 st.markdown(f'''
 <a href={url}><button style="background-color: black;">twitter</button></a>
