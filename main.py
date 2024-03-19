61c608ca8a62f0537d38da864c4d071fa85cc640
abhinavdext <163636144+abhinavdext@users.noreply.github.com>
2024-03-19 22:23:40 +0530

    Update main.py
    Ok

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ if(st.button("About")):
 
 #st.subheader("To work with perseverance,measuring up to the expectation of the company and Interested to workin team environment and contributing to the objectives of organization that offers a challenging and opportunity as a Python Developer.")
     
-st.subheader("Around 5+ years of relevant experience as a Web/Application Developer and coding with analytical programming using Python, Django. Good Experience on Automation Testing using API Automation using Python and UI Automation Experienced with full software development life-cycle,architecting scalable platforms,object-oriented programming,database design and agile methodologies.")
+    st.subheader("Around 5+ years of relevant experience as a Web/Application Developer and coding with analytical programming using Python, Django. Good Experience on Automation Testing using API Automation using Python and UI Automation Experienced with full software development life-cycle,architecting scalable platforms,object-oriented programming,database design and agile methodologies.")
 
-st.markdown("Involved in Analysis, Design, Coding, Modifications and Implementations of user requirements.")
+    st.markdown("Involved in Analysis, Design, Coding, Modifications and Implementations of user requirements.")
 
-st.header('Technical skills')
+    st.header('Technical skills')
 
-st.markdown('Web Frameworks = Django,Flask   \n   Web Technologies = Html,css  \n    Programming Language = Python  \nVersion Control = Git, GitHub  \n  Database = SQL, MongoDB, MySQL  \nIDEs/ Development Tool = Vs Code, PyCharm, Eclipse, Visual Studio, PyScripter  \nOperating Systems = Windows, Linux  \nTesting Tools = Postman, selenium  \nIssue Tracker = jira')
+    st.markdown('Web Frameworks = Django,Flask   \n   Web Technologies = Html,css  \n    Programming Language = Python  \nVersion Control = Git, GitHub  \n  Database = SQL, MongoDB, MySQL  \nIDEs/ Development Tool = Vs Code, PyCharm, Eclipse, Visual Studio, PyScripter  \nOperating Systems = Windows, Linux  \nTesting Tools = Postman, selenium  \nIssue Tracker = jira')
 
 # BMI CALCULATOR 
 st.title('Welcome to BMI Calculator')
