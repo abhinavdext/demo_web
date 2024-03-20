901861ba52141628e32eb3d52fda5168b2150e74
abhinavdext <163636144+abhinavdext@users.noreply.github.com>
2024-03-20 23:15:56 +0530

    Update main.py
    Lk

--- main.py
+++ main.py
@@ -94,22 +94,52 @@ if(st.button('Calculate BMI')):
 
         st.error("Extremely Overweight")
 
-
-########
-
-
-
-
-#url ="https://x.com/avhinavpandey?t=3aU7VK3Zelqhewv9D1158w&s=09"
-
-#st.markdown(f'''
-#<a href='url'><button style="background-color: black;">twitter</button></a>
-#''', unsafe_allow_html=True)
-
-
-
 st. write("<a href='   https://x.com/avhinavpandey?t=3aU7VK3Zelqhewv9D1158w&s=09 ' id='my-link'>twitter</a>", unsafe_allow_html=True)
 
+#######₹₹₹
 
+from tkinter import *
 
+from tkinter.ttk import *
+ 
+# importing strftime function to
+# retrieve system's time
+
+from time import strftime
+ 
+# creating tkinter window
+
+root = Tk()
+
+root.title('Clock')
+ 
+# This function is used to
+# display time on the label
+ 
+
+def time():
+
+    string = strftime('%H:%M:%S %p')
+
+    lbl.config(text=string)
+
+    lbl.after(1000, time)
+ 
+ 
+# Styling the label widget so that clock
+# will look more attractive
+
+lbl = Label(root, font=('calibri', 40, 'bold'),
+
+            background='purple',
+
+            foreground='white')
+ 
+# Placing clock at the centre
+# of the tkinter window
+
+lbl.pack(anchor='center')
+time()
+ 
+mainloop()
 
